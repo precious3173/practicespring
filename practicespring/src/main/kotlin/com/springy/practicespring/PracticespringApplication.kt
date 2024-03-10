ee4f253357ff5c53b89d1407e9f712638be5bbfe
Admin <preciouspeter3173@gmail.com>
2024-03-10 08:59:12 +0100

    Initial commit

---
+++ practicespring/src/main/kotlin/com/springy/practicespring/PracticespringApplication.kt
@@ -0,0 +1,11 @@
+package com.springy.practicespring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticespringApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticespringApplication>(*args)
+}
