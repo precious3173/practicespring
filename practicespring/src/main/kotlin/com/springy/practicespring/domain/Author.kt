646c157c201dc8eabc874973bb7421b6656763d9
Admin <preciouspeter3173@gmail.com>
2024-03-13 21:58:33 +0100

    book database class added

--- practicespring/src/main/kotlin/com/springy/practicespring/domain/Author.kt
+++ practicespring/src/main/kotlin/com/springy/practicespring/domain/Author.kt
@@ -1,4 +1,18 @@
 package com.springy.practicespring.domain
 
+import lombok.AllArgsConstructor
+import lombok.Builder
+import lombok.Data
+import lombok.NoArgsConstructor
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 class Author {
+
+    val id: Long? = null
+    val name: String? = null
+    val age: Int? = null
+
 }
--- practicespring/src/main/kotlin/com/springy/practicespring/domain/Book.kt
+++ practicespring/src/main/kotlin/com/springy/practicespring/domain/Book.kt
@@ -1,4 +1,17 @@
 package com.springy.practicespring.domain
 
+import lombok.AllArgsConstructor
+import lombok.Builder
+import lombok.Data
+import lombok.NoArgsConstructor
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 class Book {
+
+  val isbn: String? = null
+    val title: String? = null
+    val authorId: Long? = null
 }
